df29cfb15cf9fa73cb92b20dc67a29772f35b9c2
BenitoMejia <benito2471@gmail.com>
2025-10-14 08:46:57 -0600

    Updated code

---
+++ Python.py
@@ -0,0 +1,10 @@
+# String Variables 
+name = input("Type Name")
+favoritehobby = ("Hustling")
+
+# Integer Variables
+birthyear = (2003)
+currentyear = (2025)
+
+# Using f-string for concatenation 
+print(f"Hello I'm {name}, and I like {favoritehobby}")
