4977b1b554a15ecedc11fcc5c0dd8e57041c8efa
BenitoMejia <benito2471@gmail.com>
2025-11-03 14:54:42 -0700

    fixed first example

--- Python.py
+++ Python.py
@@ -1,10 +1,14 @@
 # String Variables 
-name = input("Type Name")
-favoritehobby = input("Hustling")
+name = input("Type Name:")
+favoritehobby = input("Your favorite hobby:")
 
 # Integer Variables
-birthyear = (2003)
+birthyear = input("Your birthyear:")
+birthyear = int(birthyear)
 currentyear = (2025)
 
+# Function 
+Age = currentyear - birthyear
+
 # Using f-string for concatenation 
-print(f"Hello I'm {name}, and I like {favoritehobby}")
+print(f"Hello I'm {name}, I am {Age}, and I like {favoritehobby}")
--- pwmanager.py
+++ pwmanager.py
@@ -1,16 +1,17 @@
 print("Password Generator:")
-#x is acting as a pool of data that can be randomly generated
+#x is acting as a pool of values that can be randomly generated
 import random
 x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789"
 
+#"input" will register the values the user inputs, then assign them as integers
 number = input('Number of Passwords:')
 number = int(number)
 
 length = input('Password Length:')
 length = int(length)
 
-for p in range(number):
+for p in range(number): #for loop will look for the range given by users' input
     password = " "
     for count in range(length): 
-        password += random.choice(x)
+        password += random.choice(x) #a random value from "x" is being obtained, a certain amount of times
     print(password)
