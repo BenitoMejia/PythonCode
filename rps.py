ed374de2adddcd8c9da6106eff97bf7a8f29073a
BenitoMejia <benito2471@gmail.com>
2025-11-06 11:44:20 -0700

    Progress on rock paper scissors

--- rps.py
+++ rps.py
@@ -2,4 +2,20 @@ print ("Rock, Paper, Sccisors")
 import random
 mylist = ["rock", "paper", "sccisors"]
 
-print(random.choice(mylist))
+player1 = input('rock, paper, or sccisors?:')
+player2 = random.choice(mylist)
+
+if player1 == player2:
+     print("Computer:", player2)
+     print("Draw!")
+elif player1 == 'sccisors' and player2 == 'paper':
+     print("Computer:", player2)
+     print("You Win!")
+elif player1 == 'paper' and player2 == 'rock':
+     print("Computer:", player2)
+     print("You Win!")
+elif player1 == 'rock' and player2 == 'sccisors':
+     print("Computer:", player2)
+     print("You Win!")
+else:
+     print("You lost")
